70d71859ce5669a6e0c19dd3d28d688a364f626a
Rajat Venkatesh <vrajat@users.noreply.github.com>
2019-12-10 20:11:50 +0530

    new:usr:Add schemas, tables and columns in information schema (#12)
    Information schema also processes PII in column parameters.

--- lakecli/iam/orm.py
+++ lakecli/iam/orm.py
@@ -9,6 +9,40 @@ class BaseModel(Model):
         database = database_proxy
 
 
+class Schema(BaseModel):
+    class Meta:
+        table_name = 'schemata'
+
+    id = AutoField()
+    schema_name = TextField()
+    location = TextField(null=True)
+
+
+class Table(BaseModel):
+    class Meta:
+        table_name = 'tables'
+
+    id = AutoField()
+    table_schema = TextField()
+    table_name = TextField()
+    create_time = TextField(null=True)
+    last_access_time = TextField(null=True)
+
+
+class Column(BaseModel):
+    class Meta:
+        table_name = 'columns'
+
+    id = AutoField()
+    table_schema = TextField()
+    table_name = TextField()
+    column_name = TextField()
+    data_type = TextField()
+    is_partition_column = BooleanField()
+    pii = TextField(null=True)
+    ordinal = IntegerField()
+
+
 class DatabasePrivilege(BaseModel):
     class Meta:
         table_name = 'database_privileges'
@@ -36,7 +70,7 @@ def init(path):
     database = SqliteDatabase(os.path.expanduser(path))
     database_proxy.initialize(database)
     database_proxy.connect()
-    database_proxy.create_tables([DatabasePrivilege, TablePrivilege])
+    database_proxy.create_tables([DatabasePrivilege, TablePrivilege, Schema, Table, Column])
 
     return database_proxy
 
--- lakecli/iam/scanner.py
+++ lakecli/iam/scanner.py
@@ -2,8 +2,7 @@ import os
 import boto3
 import logging
 
-from lakecli.iam.orm import TablePrivilege, DatabasePrivilege, init, model_db_close
-
+from lakecli.iam.orm import TablePrivilege, DatabasePrivilege, init, model_db_close, Table, Database, Schema, Column
 
 LOGGER = logging.getLogger(__name__)
 
@@ -16,15 +15,43 @@ class Scanner:
             LOGGER.info("Remove old sqlite database at %s" % self.path)
             os.remove(self.path)
 
+    @staticmethod
+    def parse_columns(column_list, ordinal_no, is_partition_col, schema_name, table_name):
+        LOGGER.debug(column_list)
+        LOGGER.info("%d columns found in %s.%s" % (
+            len(column_list), schema_name, table_name))
+        columns = []
+        for c in column_list:
+            pii = c['Parameters']['PII'] if 'Parameters' in c and 'PII' in c['Parameters'] else None
+            columns.append(Column(
+                table_schema=schema_name,
+                table_name=table_name,
+                column_name=c['Name'],
+                data_type=c['Type'],
+                is_partition_column=is_partition_col,
+                pii=pii,
+                ordinal=ordinal_no
+            ))
+            ordinal_no += 1
+
+        return columns, ordinal_no
+
     def scan(self):
         lake_client = boto3.client('lakeformation',
                                    region_name=self.aws_config.region,
                                    aws_access_key_id=self.aws_config.aws_access_key_id,
                                    aws_secret_access_key=self.aws_config.aws_secret_access_key)
 
-        table_permissions = lake_client.list_permissions(
-            ResourceType='TABLE',
-        )
+        table_permissions = []
+        next_token = ''
+
+        while next_token is not None:
+            result = lake_client.list_permissions(
+                ResourceType='TABLE',
+                NextToken=next_token
+            )
+            table_permissions += result['PrincipalResourcePermissions']
+            next_token = result['NextToken'] if 'NextToken' in result else None
 
         LOGGER.debug(table_permissions)
 
@@ -34,8 +61,8 @@ class Scanner:
         LOGGER.debug(database_permissions)
 
         table_privileges = []
-        LOGGER.info("%d table permissions found." % len(table_permissions['PrincipalResourcePermissions']))
-        for resource in table_permissions['PrincipalResourcePermissions']:
+        LOGGER.info("%d table permissions found." % len(table_permissions))
+        for resource in table_permissions:
             for permission in resource['Permissions']:
                 grant = permission in resource['PermissionsWithGrantOption']
                 table_privileges.append(self._new_privilege(permission, resource['Resource'],
@@ -54,18 +81,70 @@ class Scanner:
                     permission=permission, grant=grant
                 ))
 
+        glue_client = boto3.client('glue',
+                                   region_name=self.aws_config.region,
+                                   aws_access_key_id=self.aws_config.aws_access_key_id,
+                                   aws_secret_access_key=self.aws_config.aws_secret_access_key)
+
+        schemata = []
+        tables = []
+        columns = []
+
+        schemata_response = glue_client.get_databases()
+        LOGGER.debug(schemata_response)
+        LOGGER.info("%d databases found " % len(schemata_response['DatabaseList']))
+        for d in schemata_response['DatabaseList']:
+
+            schemata.append(Schema(
+                schema_name=d['Name'],
+                location=d['LocationUri'] if 'LocationUri' in d else None
+            ))
+
+            tables_response = glue_client.get_tables(
+                DatabaseName=d['Name']
+            )
+            LOGGER.debug(tables_response)
+            LOGGER.info("%d tables found in %s" % (len(tables_response['TableList']), d['Name']))
+            for t in tables_response['TableList']:
+                tables.append(Table(
+                    table_schema=d['Name'],
+                    table_name=t['Name'],
+                    create_time=t['CreateTime'],
+                    last_access_time=t['LastAccessTime'] if 'LastAccessTime' in t else None,
+                ))
+
+                ordinal_no = 1
+                if 'StorageDescriptor' in t:
+                    for key in ('PartitionKeys', 'Columns'):
+                        if key in t['StorageDescriptor']:
+                            column_list = t['StorageDescriptor'][key]
+                            parsed_columns, ordinal_no = Scanner.parse_columns(column_list,
+                                                                               ordinal_no,
+                                                                               key == 'PartitionKeys',
+                                                                               d['Name'], t['Name'])
+                            columns.extend(parsed_columns)
+
         connection = init(self.path)
         try:
             with connection.atomic():
                 TablePrivilege.bulk_create(table_privileges, batch_size=100)
                 DatabasePrivilege.bulk_create(db_privileges, batch_size=100)
+                Schema.bulk_create(schemata, batch_size=100)
+                Table.bulk_create(tables, batch_size=100)
+                Column.bulk_create(columns, batch_size=100)
+
         finally:
             model_db_close()
 
     @staticmethod
     def _principal(arn):
         # http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
+        LOGGER.debug(arn)
         elements = arn.split(':', 5)
+        if len(elements) != 6:
+            LOGGER.warning("Unknown format for arn '%s'" % arn)
+            return arn
+
         result = {
             'arn': elements[0],
             'partition': elements[1],
